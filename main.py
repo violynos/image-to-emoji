2864ea84b9d74b86c1b9b3697fda14af160f1f1c
vio <111750437+violynos@users.noreply.github.com>
2025-06-21 01:02:32 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import PIL
+from PIL import Image
+
+#-----------------------------------------------
+#  
+#  max width is 53; height dont matter
+#  max char count 2k(def) or 4k
+#  
+#  
+#  
+#  
+#  
+#  
+#  
+#  
+#  
+#-----------------------------------------------
+
+InImg = input("What image should i use? ")
